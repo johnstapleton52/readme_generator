32f2b1cb4444011e083ec92ce69481108bed879f
John Stapleton <johnstapleton52@gmail.com>
2021-10-10 14:31:47 -0700

    moved files to my personal repositorepository

---
+++ README.md
@@ -0,0 +1,43 @@
+
+    # John's Proj
+
+    ## Description
+    ;
+
+
+    ## Table of Contents
+    [Installation](###### Installation)
+    [Usage](###### Usage)
+    [License](###### License)
+    [Contributing](###### Contributing)
+    [Tests](###### Tests)
+    [Questions](###### Questions)
+
+
+
+    ###### Installation
+    sdfa
+
+
+    ###### Usage
+    asdf
+
+
+    ###### License
+    
+
+
+    ###### Contributing
+    undefined
+
+
+    ###### Tests
+    sdfa
+
+
+    ###### Questions
+    email: [dasf](dasf)
+    GitHub: [](https://github.com/)
+
+   
+    
---
+++ source/template.js
@@ -0,0 +1,46 @@
+module.exports = (data) => {
+    return `
+    # ${data.project_title}
+
+    ## Description
+    ${data.description}
+
+
+    ## Table of Contents
+    [Installation](###### Installation)
+    [Usage](###### Usage)
+    [License](###### License)
+    [Contributing](###### Contributing)
+    [Tests](###### Tests)
+    [Questions](###### Questions)
+
+
+
+    ###### Installation
+    ${data.installation_instructions}
+
+
+    ###### Usage
+    ${data.usage_information}
+
+
+    ###### License
+    ${data.license}
+
+
+    ###### Contributing
+    ${data.contibution_guidelines}
+
+
+    ###### Tests
+    ${data.test_instructions}
+
+
+    ###### Questions
+    email: [${data.email}](${data.email})
+    GitHub: [${data.github_username}](https://github.com/${data.github_username})
+
+   
+    `;
+
+};
